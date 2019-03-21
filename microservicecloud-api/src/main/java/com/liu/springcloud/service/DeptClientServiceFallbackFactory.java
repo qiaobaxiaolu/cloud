2984c3d8df6c5725da32e0f46c6c00b85a3eaaab
liujiarui <15229106888@163.com>
2019-03-21 21:47:26 +0800

    no message

--- microservicecloud-api/src/main/java/com/liu/springcloud/service/DeptClientServiceFallbackFactory.java
+++ microservicecloud-api/src/main/java/com/liu/springcloud/service/DeptClientServiceFallbackFactory.java
@@ -9,28 +9,27 @@ import com.liu.springcloud.entity.Dept;
 import feign.hystrix.FallbackFactory;
 
 @Component // 不要忘记添加，不要忘记添加
-public class DeptClientServiceFallbackFactory implements FallbackFactory<DeptClientService>
-{
+public class DeptClientServiceFallbackFactory implements FallbackFactory<DeptClientService> {
 	@Override
-	public DeptClientService create(Throwable throwable)
-	{
+	public DeptClientService create(Throwable throwable) {
 		return new DeptClientService() {
 			@Override
-			public Dept get(long id)
-			{
-				return new Dept().setDeptNo(id).setDname("该ID：" + id + "没有没有对应的信息,Consumer客户端提供的降级信息,此刻服务Provider已经关闭")
-						.setDb_source("no this database in MySQL");
+			public Dept get(long id) {
+				Dept dept = new Dept();
+				dept.setDeptNo(id);
+				dept.setDeptName("该ID：" + id + "没有没有对应的信息,Consumer客户端提供的降级信息,此刻服务Provider已经关闭");
+				dept.setDbSource("no this database in MySQL");
+				return dept;
+						
 			}
 
 			@Override
-			public List<Dept> list()
-			{
+			public List<Dept> list() {
 				return null;
 			}
 
 			@Override
-			public boolean add(Dept dept)
-			{
+			public boolean add(Dept dept) {
 				return false;
 			}
 		};
